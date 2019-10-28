43d708a25e057dbaa4e8316084766bcb9ba55238
a.neposedov <a.neposedov@concert.ua>
2019-10-28 12:39:30 +0200

    HelpController created; MetaTag updated; PostController updated; CarInfoPageController updated;

--- resources/views/staticPages/contact-us.blade.php
+++ resources/views/staticPages/contact-us.blade.php
@@ -7,7 +7,7 @@
         </div> <!-- end container -->
         <div class="container">
             {!! $content->where('section', 'map_title')->first()->getText() !!}
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2539.73711179624!2d30.4611551159444!3d50.46461989422446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cdd3da590379%3A0xbe23bf1751e2cbda!2z0YPQuy4g0KHQtdC80YzQuCDQpdC-0YXQu9C-0LLRi9GFLCA4LCDQmtC40LXQsiwgMDIwMDA!5e0!3m2!1sru!2sua!4v1570109865305!5m2!1sru!2sua"
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1958.2144446019686!2d30.457966860042657!3d50.46922295651822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cde79e9da45d%3A0xb4dbcde25dacf93f!2zVHdpbnR1cmJvIC0g0YDQtdC80L7QvdGCINGC0YPRgNCx0LjQvQ!5e0!3m2!1suk!2sua!4v1572259104152!5m2!1suk!2sua"
                     width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
     </section>
