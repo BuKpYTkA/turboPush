77dc2dc4e36806887be68cf78a2546ee04ffe89b
a_neposedov <anton@mylearninghub.com>
2021-02-08 15:09:48 +0200

    logos updated;

--- resources/views/layouts/components/head.blade.php
+++ resources/views/layouts/components/head.blade.php
@@ -13,8 +13,8 @@
     <meta property="og:description" content="{{ $metaTagContent->getOgDescription() }}">
     <meta property="og:image" content="{{ $bannerImage->getImageUrl() }}">
 
-    <link rel="shortcut icon" href="{{ asset('images') }}/favicon.png">
-    <link rel="apple-touch-icon" href="{{ asset('images') }}/favicon.png">
+    <link rel="shortcut icon" href="{{ asset('images') }}/favicon.ico">
+    <link rel="apple-touch-icon" href="{{ asset('images') }}/favicon.ico">
 
     <link rel="stylesheet" href="{{ asset('frontEnd') }}/styles/vendor.css">
 
