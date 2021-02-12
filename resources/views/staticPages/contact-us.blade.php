4a979c5b53048f77d4e0bd530b7a64a96a8cb3a1
a_neposedov <anton@mylearninghub.com>
2021-02-12 15:47:59 +0200

    map update;

--- resources/views/staticPages/contact-us.blade.php
+++ resources/views/staticPages/contact-us.blade.php
@@ -15,7 +15,7 @@
                 @include('layouts.components.contact-form')
             </div>
             <div class="col-md-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1958.2144446019686!2d30.457966860042657!3d50.46922295651822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cde79e9da45d%3A0xb4dbcde25dacf93f!2zVHdpbnR1cmJvIC0g0YDQtdC80L7QvdGCINGC0YPRgNCx0LjQvQ!5e0!3m2!1suk!2sua!4v1572259104152!5m2!1suk!2sua"
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1269.6345642813762!2d30.438310140552705!3d50.473333295268304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTDCsDI4JzI0LjAiTiAzMMKwMjYnMjEuNyJF!5e0!3m2!1sen!2sua!4v1613137606257!5m2!1sen!2sua"
                         width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
             </div>
         </div>
